84c91fbe788c23c88aba3d61ad7109c9fe87799b
Mikhail <mishnade@gmail.com>
2016-05-10 12:13:48 -0700

    passing first test

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 "use strict"
+
+
 console.log('I am alive');
+
---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+"use strict"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Voting from './components/Voting';
+
+console.log('I am alive');
+
+const pair = ['Trainspotting', '28 Days Later'];
+
+ReactDOM.render(
+  <Voting pair={pair} />,
+  document.getElementById('app')
+);
