6d9f9248abd77806f967d377c26baa319fecc503
Mikhail <mishnade@gmail.com>
2016-05-12 10:41:44 -0700

    add redux store

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { Router, Route, hashHistory } from 'react-router';
 import Voting from './components/Voting';
 import Results from './components/Results';
@@ -9,12 +11,25 @@ import Results from './components/Results';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
+const store = createStore(reducer);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Sunshine', '28 Days Later'],
+      tally: { Sunshine: 2}
+    }
+  }
+});
+
 const routes = <Route component={App}>
   <Route path="/results" component={Results} />
   <Route path="/" component={Voting} />
 </Route>;
 
 ReactDOM.render(
-  <Router history={hashHistory}>{routes}</Router>,
+  <Provider store={store}>
+    <Router history={hashHistory}>{routes}</Router>
+  </Provider>,
   document.getElementById('app')
 );
