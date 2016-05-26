df7622b82070b6a8bf9c491c2ebf60d0ff816d24
Mikhail <mishnade@gmail.com>
2016-05-25 20:28:28 -0700

    refactor

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -6,7 +6,7 @@ import {
   scryRenderedDOMComponentsWithTag,
   Simulate
 } from 'react-addons-test-utils';
-import {List} from 'immutable';
+import { List } from 'immutable';
 import { Voting } from  '../../src/components/Voting';
 import { expect } from 'chai';
 
